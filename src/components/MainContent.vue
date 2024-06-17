bef4f16a2467819ef2ca2d930c78e3fa829c0463
alvin jiang <764199809@qq.com>
2024-06-17 02:37:27 +0000

    feat: 配置样式

---
+++ src/components/MainContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <div class="phone-block-container">
+      <div class="phone-block left-block"></div>
+      <div class="phone-block right-block"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.main {
+  width: 100%;
+  height: 1000px;
+  background: #000;
+  .phone-block-container {
+    width: 80vw;
+    height: 500px;
+    background: red;
+    margin: 0 auto;
+  }
+  .phone-block {
+    width: '40%';
+    height: 200px;
+    background: yellow;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,20 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/remotecontrol.github.io/' // 替换为你的 GitHub 仓库名称
-    : '/'
+    : '/',
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            // 自定义主题变量
+            'primary-color': '#1DA57A',
+            'link-color': '#1DA57A',
+            'border-radius-base': '2px',
+          },
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
 })
